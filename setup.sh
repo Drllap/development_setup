1615f3ba1d7be127cb1602f6f27fe93abad12442
Pall Palsson <drllap@gmail.com>
2021-03-20 19:53:41 +0000

    Add setup.sh to install neovim and init.vim for it

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# install daily build for neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt install neovim
+
+# install vim-plug
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s `pwd`/NeoVim/init.vim ~/.config/nvim/init.vim
+
+# setup git
+git config --global user.email "drllap@gmail.com"
+git config --global user.name "Pall Palsson"
+git config --global core.editor "nvim"  
