dce1785fe4c47719fd8d9818ce7a4721d222842f
Pall Palsson <palli@kvikna.com>
2021-08-27 17:07:51 +0000

    Make new Windows Termanal panes be spawned in current directory

--- WindowsTerminal/powershell_profile.ps1
+++ WindowsTerminal/powershell_profile.ps1
@@ -18,3 +18,22 @@ function so {
     echo "re-sourcing profile: " $PROFILE.CurrentUserAllHosts
 }
 
+# Code taken from https://github.com/microsoft/terminal/issues/3158#issuecomment-789198188
+# Makes make new split panes open in current directory
+# My changes:
+#       Instead of executing "$prevprompt" explicitly execute the prompt from posh-git
+#       This is done because posh-git doesn't overwrite the prompt function if it
+#       already exists.
+$esc = [char]27
+if($env:WT_SESSION){
+	# $prevprompt = $Function:prompt
+	function prompt {
+		if ($pwd.provider.name -eq "FileSystem") {
+			$p = $pwd.ProviderPath
+			Write-host "$esc]9;9;`"$p`"$esc\" -NoNewline
+		}
+		# return $prevprompt.invoke()
+                return $GitPromptScriptBlock.Invoke()
+	}
+}
+
