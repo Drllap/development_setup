bd8b956cd08c139eba755914af41817b56f797a9
Palli Pals <drllap@gmail.com>
2022-01-23 01:00:21 +0000

    Add code to my PowerShell profile that make j,k exit insert mode like it does in Neovim

--- WindowsTerminal/powershell_profile.ps1
+++ WindowsTerminal/powershell_profile.ps1
@@ -194,6 +194,27 @@ $Script:PSReadLineOptions = @{
     ViModeChangeHandler = $Function:OnViModeChange
 }
 Set-PSReadLineOption @PSReadLineOptions
+
+
+$j_timer = New-Object System.Diagnostics.Stopwatch
+
+Set-PSReadLineKeyHandler -Key j -ViMode Insert -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("j")
+    $j_timer.Restart()
+}
+
+Set-PSReadLineKeyHandler -Key k -ViMode Insert -ScriptBlock {
+    if (!$j_timer.IsRunning -or $j_timer.ElapsedMilliseconds -gt 1000) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("k")
+    } else {
+        [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor, 1)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor-1)
+    }
+}
     
 
 
