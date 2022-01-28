4cb9a19d23a8eb1b4115178867ba1d1d2e77af27
Palli Pals <drllap@gmail.com>
2022-01-28 13:10:01 +0000

    Add some commented out code to the PowerShell profile to select the PSReadLine version

--- WindowsTerminal/powershell_profile.ps1
+++ WindowsTerminal/powershell_profile.ps1
@@ -103,6 +103,8 @@ function so {
 }
 
 Import-Module PSReadLine -RequiredVersion "2.2.0"
+# Remove-Module PSReadLine -Force
+# Import-Module PSReadLine -RequiredVersion "2.1.0"
 
 Import-Module posh-git  # posh-git, git info in prompt and auto tab completion
 
