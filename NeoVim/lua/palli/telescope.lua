aa1d212726a4f6ca4c93c25d1cec200a6f6d7ff6
Palli Pals <drllap@gmail.com>
2022-03-21 22:58:41 +0000

    Some Telescope setup changes
    * Order buffers in "most reasent used" order
    * Add keymap for find_files with hidden/ignored files included
    * Add grep_string keymap

--- NeoVim/lua/palli/telescope.lua
+++ NeoVim/lua/palli/telescope.lua
@@ -5,9 +5,12 @@ require('telescope').load_extension('fzf')
 local opts = { noremap = true, silent = true, expr = false };
 local set_keymap = vim.api.nvim_set_keymap;
 set_keymap('n', '<leader>tf', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts);
-set_keymap('n', '<leader>tb', '<cmd>lua require("telescope.builtin").buffers()<cr>', opts);
+set_keymap('n', '<leader>tb', '<cmd>lua require("telescope.builtin").buffers({ sort_mru = true })<cr>', opts);
 set_keymap('n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<cr>', opts);
 set_keymap('n', '<leader>tw', '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts);
+set_keymap('n', '<leader>ts', '<cmd>lua require("telescope.builtin").grep_string()<cr>', opts);
+
+set_keymap('n', '<leader>tF', '<cmd>lua require("telescope.builtin").find_files({ hidden = true, no_ignore = true })<cr>', opts);
 
 -- Create keymap to browse the vim config files with Telescope
 local search_root = { cwd = vim.fn.fnamemodify(vim.fn.resolve(vim.env.MYVIMRC), ':h:h'), };
