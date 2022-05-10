c9735ae6866bc56e2dbbcd4db2c111c6cf199ad3
Palli Pals <drllap@gmail.com>
2022-05-10 22:57:36 +0000

    Add LSP keybindings

--- NeoVim/lua/lsp/init.lua
+++ NeoVim/lua/lsp/init.lua
@@ -4,3 +4,22 @@ require('lsp.python')
 require('lsp.cmake')
 require('lsp.vim')
 require('lsp.cpp')
+
+vim.keymap.set('n', '<leader>cR', vim.lsp.buf.rename)
+vim.keymap.set('n', '<leader>cA', vim.lsp.buf.code_action)
+
+vim.keymap.set('n', '<leader>ch', vim.lsp.buf.signature_help)
+vim.keymap.set('n', '<leader>cK', vim.lsp.buf.hover)
+
+vim.keymap.set('n', '<leader>cd', vim.lsp.buf.declaration)
+vim.keymap.set('n', '<leader>cD', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>ct', vim.lsp.buf.type_definition)
+vim.keymap.set('n', '<leader>ci', vim.lsp.buf.implementation)
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.references)
+vim.keymap.set('n', '<leader>cs', vim.lsp.buf.document_symbol)
+vim.keymap.set('n', '<leader>cS', vim.lsp.buf.workspace_symbol)
+
+vim.keymap.set('n', '<leader>cc', vim.lsp.buf.incoming_calls)
+vim.keymap.set('n', '<leader>cC', vim.lsp.buf.outgoing_calls)
+
+
