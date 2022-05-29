ee87974c825c51a108e3b82f4ad014bfb41ac74b
Pall Palsson <drllap@gmail.com>
2022-05-29 15:48:28 +0000

    Install Neovim by building the source

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 set -e
 
 # install daily build for neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable
+# sudo add-apt-repository ppa:neovim-ppa/unstable
+# now installing from source, see below
 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
@@ -12,14 +13,19 @@ sudo apt install    \
   git               \
   gh                \
   npm               \
-  neovim            \
+#  neovim            \ building from source, see below
   ripgrep           \
   fd-find           \
   python3           \
   python3-pip       \
   ninja-build       \
   stow              \
-  manpages-posix
+  manpages-posix    \
+  build-essential   \
+  libtool           \
+  autoconf          \
+  unzip             \
+  wget              \
   clangd-12
 
 pip3 install            \
@@ -43,4 +49,13 @@ compile/install.sh
 cd ../..
 ./3rd/luamake/luamake rebuild
 popd
+
+
+# install neovim from source
+git clone git@github.com:neovim/neovim.git ../neovim
+pushd ../neovim
+make CMAKE_BUILD_TYPE=RelWithDebINfo
+sudo make install
+popd
+
 
