b9c050edebc950cd4b5be3f68f7b17d15872cba9
Pall Palsson <palli@kvikna.com>
2022-12-02 16:45:03 +0000

    Some telescope-tabs config fixes
    * Add keybinding to go_to_previous -> <leader>pt
    * Add config for entry_ordinal, this controls how the fuzzy search works
    ** https://github.com/LukasPietzschmann/telescope-tabs/issues/5#issuecomment-1335471234

--- NeoVim/lua/palli/telescope-tabs.lua
+++ NeoVim/lua/palli/telescope-tabs.lua
@@ -1,6 +1,13 @@
-local ef = function(tab_id, buffer_ids, file_names, file_paths)
+local list_cwds = function(tab_id, buffer_ids, file_names, file_paths)
   return vim.fn.getcwd(-1, vim.api.nvim_tabpage_get_number(tab_id))
 end
 
-vim.keymap.set('n', '<leader>tt', function() require('telescope-tabs').list_tabs({ entry_formatter = ef}) end)
+local list_cwds = function(tab_id, buffer_ids, file_names, file_paths)
+  return vim.fn.getcwd(-1, vim.api.nvim_tabpage_get_number(tab_id))
+end
+
+local config = { entry_formatter = list_cwds, entry_ordinal = list_cwds }
+
+vim.keymap.set('n', '<leader>tt', function() require('telescope-tabs').list_tabs(config) end)
+vim.keymap.set('n', '<leader>pt', function() require('telescope-tabs').go_to_previous() end);
 
