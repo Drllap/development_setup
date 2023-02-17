9d6da555a5f5cd26749f0d0a052577c6f3b334a6
Palli Pals <drllap@gmail.com>
2023-02-17 00:23:06 +0000

    Add toml Treesitter parser

--- NeoVim/lua/palli/treesitter.lua
+++ NeoVim/lua/palli/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = { "c", "cpp", "cmake", "dockerfile", "lua", "vim", "python",
-                       "json", "yaml", "markdown" },
+                       "json", "yaml", "toml", "markdown" },
 
   highlight = {
     enable = true,
