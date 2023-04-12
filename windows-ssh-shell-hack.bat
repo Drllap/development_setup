a43f3b88a6174d81ceef9681d3813a7bfcf5ece8
Pall Palsson <palli@kvikna.com>
2023-04-12 09:36:21 +0000

    Add batch file hack to fix windows ssh command args issue

---
+++ windows-ssh-shell-hack.bat
@@ -0,0 +1,12 @@
+@echo off
+powershell -NoProfile -NoLogo %*
+
+REM This file is a hack to resolve the issue that Win32-OpenSSH doesn't (didn't) respect the
+REM DefaultShellArguments/DefaultShellCommandOption registers
+REM
+REM How to install this batch file as the default "shell" when connecting to ssh:
+REM sudo Set-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value (Get-ChildItem .\windows-ssh-shell-hack.bat | % { $_.FullName })
+REM
+REM See this issue/comments for details:
+REM https://github.com/PowerShell/Win32-OpenSSH/issues/1677#issuecomment-709525286
+REM https://github.com/PowerShell/Win32-OpenSSH/issues/1677#issuecomment-1499545946&
