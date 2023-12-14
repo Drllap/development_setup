cd4ab594f2ec332cfe02aa4276b22bc4772f80a1
Palli Pals <drllap@gmail.com>
2023-12-14 20:56:48 +0000

    NeoVim: Remove duplicate function definition
    The list_cwds was defined twice in the config for telescope-tabs, remove
    one of them

--- NeoVim/lua/palli/telescope-tabs.lua
+++ NeoVim/lua/palli/telescope-tabs.lua
@@ -2,10 +2,6 @@ local list_cwds = function(tab_id, buffer_ids, file_names, file_paths)
   return vim.fn.getcwd(-1, vim.api.nvim_tabpage_get_number(tab_id))
 end
 
-local list_cwds = function(tab_id, buffer_ids, file_names, file_paths)
-  return vim.fn.getcwd(-1, vim.api.nvim_tabpage_get_number(tab_id))
-end
-
 local config = { entry_formatter = list_cwds, entry_ordinal = list_cwds }
 
 vim.keymap.set('n', '<leader>tt', function() require('telescope-tabs').list_tabs(config) end)
