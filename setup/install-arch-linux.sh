e5a9d9959c9bedeec7814c9301a10f9e5187826d
Pall Palsson <drllap@gmail.com>
2024-01-04 00:10:05 +0000

    Setup: Add Arch Linux
    * Add setup script for Arch Linux
    * Rename old setup script to install-ubuntu

---
+++ setup/install-arch-linux.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+pacman -Su                  \
+    sudo                    \
+    vim                     \
+    neovim                  \
+    neovide                 \
+    git                     \
+    github-cli              \
+    bash-completion         \
+    bash-language-server    \
+    wezterm                 \
+    curl                    \
+    signal-desktop          \
+    cmake                   \
+    tree                    \
+    fd                      \
+    ripgrep                 \
+    clang                   \
+    pyright                 \
+    alacritty               \
+    zellij                  \
+    zoxide                  \
+    vlc                     \
+    fzf                     \
+    xclip                   \
+    npm                     \
+    lua-language-server     \
+    stow                    \
+    rustup                  \
+    zig                     \
+    zls                     \
+    nushell
+
+npm install -g 
+    vim-language-server
+
+# stow everything, execute from repo root
+stow --target=$HOME bash
+stow --target=$HOME git
+stow --target=$HOME wezterm
+stow --target=$HOME readline
+stow --target=$HOME conan
+stow --target=$HOME nushell
+
