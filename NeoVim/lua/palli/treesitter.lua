17b4137baef066e59472cb89afb2eb80caf7b0b8
Pall Palsson <palli@kvikna.com>
2024-02-27 10:27:09 +0000

    NeoVim: Add vimdoc to TreeSitter

--- NeoVim/lua/palli/treesitter.lua
+++ NeoVim/lua/palli/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = { "c", "cpp", "rust", "cmake", "dockerfile", "lua", "vim", "python",
-                       "json", "yaml", "toml", "markdown", "bash", },
+                       "json", "yaml", "toml", "markdown", "bash",  "vimdoc", },
 
   highlight = {
     enable = true,
