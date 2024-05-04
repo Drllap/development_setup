7c715456a96a0900c3dc03db749c6f10fb7761a1
Pall Palsson <drllap@gmail.com>
2024-05-04 20:26:31 +0000

    Bash: Use the new fzf init feature
    * Use fd with fzf instead of find
    * Use `fzf --bash` to init the fzf bindings
    * Create my own binding instead of the default ALT+c for fzf cd

--- scripts/bash/fzf.sh
+++ scripts/bash/fzf.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-source /usr/share/fzf/completion.bash
-source /usr/share/fzf/key-bindings.bash
+FZF_CTRL_T_COMMAND=fd
+FZF_ALT_C_COMMAND=""    # because I want my own bindings
+
+eval "$(fzf --bash)"
+
+# Use CTRL+g instead of ALT+c for fzf-cd-ing to subfolder
+bind -m emacs-standard '"\C-g": " \C-b\C-k \C-u`__fzf_cd__`\e\C-e\er\C-m\C-y\C-h\e \C-y\ey\C-x\C-x\C-d"'
+bind -m vi-command '"\C-g": "\C-z\C-g\C-z"'
+bind -m vi-insert '"\C-g": "\C-z\C-g\C-z"'
+
 
