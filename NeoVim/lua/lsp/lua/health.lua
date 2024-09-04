4dfa1db2aa180ab734eb6cd305c5d47bb9a4974d
Pall Palsson <palli@kvikna.com>
2024-09-04 12:48:21 +0000

    Fix WARNING when using depricated health API

--- NeoVim/lua/lsp/lua/health.lua
+++ NeoVim/lua/lsp/lua/health.lua
@@ -11,18 +11,18 @@ local lsp_srv_env_var_set = function()
 end
 
 M.check = function()
-  vim.health.report_start("Lua LSP report")
+  vim.health.start("Lua LSP report")
 
   if lsp_srv_env_var_set() then
-    vim.health.report_ok("LSP_Servers environmental variable set: " .. vim.env.LSP_Servers)
+    vim.health.ok("LSP_Servers environmental variable set: " .. vim.env.LSP_Servers)
   else
-    vim.health.report_error("environmental variable 'LSP_Servers' not set")
+    vim.health.error("environmental variable 'LSP_Servers' not set")
   end
 
   if check_has_server() then
-    vim.health.report_ok("Lua LSP server executable found at: " .. utils.lua_lsp_binary())
+    vim.health.ok("Lua LSP server executable found at: " .. utils.lua_lsp_binary())
   else
-    vim.health.report_error("Lua LSP server executable not found, was supposed to be at: " .. utils.lua_lsp_binary())
+    vim.health.error("Lua LSP server executable not found, was supposed to be at: " .. utils.lua_lsp_binary())
   end
 end
 
--- NeoVim/lua/lsp/vim/health.lua
+++ NeoVim/lua/lsp/vim/health.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 M.check = function()
-  vim.health.report_start("Vim LSP report")
+  vim.health.start("Vim LSP report")
 
   if 1 == vim.fn.executable("vim-language-server") then
-    vim.health.report_ok("Found vim-language-server")
+    vim.health.ok("Found vim-language-server")
   else
-    vim.health.report_error("Can't find vim-language-server executable. Install with `npm install -g vim-language-server`")
+    vim.health.error("Can't find vim-language-server executable. Install with `npm install -g vim-language-server`")
   end
 end
 return M
