0a87dafa55b259d1267a04eb70268c72ce9c5ecd
Pall Palsson <palli@kvikna.com>
2025-05-19 17:05:06 +0000

    NeoVim: Switch the gruvbox theme plugin
    * Freom gruvbox-community/gruvbox to ellisonleao/gruvbox.nvim

--- NeoVim/lua/config/lazy.lua
+++ NeoVim/lua/config/lazy.lua
@@ -26,10 +26,11 @@ require("lazy").setup({
   spec = {
     -- Color schemes
     {
-      'gruvbox-community/gruvbox',
+      'ellisonleao/gruvbox.nvim',
       lazy = false,
       priority = 1000,
       config = function()
+        vim.o.background = "dark"
         vim.cmd([[colorscheme gruvbox]])
       end,
     },
