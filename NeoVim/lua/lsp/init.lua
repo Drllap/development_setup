8f4df585c34f557ec618f7dcb61443e9cfd3da9f
Pall Palsson <drllap@gmail.com>
2025-10-21 23:17:35 +0000

    NeoVim: Move Lua lsp to the new API

--- NeoVim/lua/lsp/init.lua
+++ NeoVim/lua/lsp/init.lua
@@ -1,4 +1,3 @@
--- pcall(require,'lsp.lua_ls')
 -- pcall(require,'lsp.PowerShell')
 -- pcall(require,'lsp.cpp')
 
@@ -22,6 +21,53 @@ vim.lsp.config('clangd', {
     vim.keymap.set('n', 'gh',  "<cmd>LspClangdSwitchSourceHeader<cr>", { buffer = true, })
   end,
 })
+
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library'
+          -- '${3rd}/busted/library'
+        }
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = {
+        --   vim.api.nvim_get_runtime_file('', true),
+        -- }
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+})
+
 vim.lsp.config('cspell_ls', {
     filetypes = {"go", "rust", "js", "ts", "html", "css", "json", "yaml", "markdown", "gitcommit", "cpp", "c" },
 })
