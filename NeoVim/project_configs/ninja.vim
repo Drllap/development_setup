6020267bbf46cbfb7fe33d8825598952f500ee13
Pall Palsson <palli@kvikna.com>
2025-11-07 09:39:32 +0000

    NeoVim: Add project_configs files

---
+++ NeoVim/project_configs/ninja.vim
@@ -0,0 +1,9 @@
+echo "ninja"
+compiler! gcc
+set makeprg=ninja
+" noremap <leader>b :silent! :wall <bar> :execute ':Make ' .. '*.sln'->globpath('../build/')<CR>
+noremap <leader>b :silent! :wall <bar> :Dispatch -dir=./build/Debug/<CR>
+noremap <leader>B :silent! :wall <bar> :Dispatch -dir=./build/Debug/ -- -k 0<CR>
+command! -nargs=0 CMake !cmake -G Ninja -S ./src/ -B ./build/ -DCMAKE_MODULE_PATH=${PWD}/build/ -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Debug
+:silent! !ln --symbolic --force ../build/Debug/compile_commands.json ./src/compile_commands.json
+
---
+++ NeoVim/project_configs/wbms_gui_config.lua
@@ -0,0 +1,26 @@
+-- opts = { noremap = true, silent = true, expr = false };
+vim.keymap.set('n', '<leader>tC', function() 
+  local opts = { search_dirs = { "~/.config/Norbit"} }
+  return require('telescope.builtin').find_files(opts)
+end,
+-- function() return require('telescope.builtin').find_files("~/AppData/Roaming/Norbit") end, 
+{ noremap = true, silent = true, expr = false })
+
+
+local dap = require("dap")
+dap.configurations.cpp = {
+  {
+      name = "Launch",
+      type = "gdb",
+      request = "launch",
+      program = function()
+        local ret = vim.fn.getcwd() .. '/build/output/wbmsgui'
+        vim.print("Starting gdb with: " .. ret)
+        return ret
+      end,
+      args = {}, -- provide arguments if needed
+      cwd = "${workspaceFolder}",
+      stopAtBeginningOfMainSubprogram = false,
+  },
+}
+
