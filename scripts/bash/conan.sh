79e6fa3e65e4b3c201e9be270af9ae9f9d3f6a79
Pall Palsson <drllap@gmail.com>
2026-02-16 10:45:44 +0000

    Conan: enable revisions
    * add CONAN_REVISIONS_ENABLED=1 to profile

--- scripts/bash/conan.sh
+++ scripts/bash/conan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export CONAN_REVISIONS_ENABLED=1
+
 function conan_connect {
-    conannor user pallp -p $NORBIT_GITLAB_BASIC_TOKEN -r gitlab-conan-project
-    conannor user pallp -p $NORBIT_GITLAB_BASIC_TOKEN -r gitlab
+    conan user pallp -p $NORBIT_GITLAB_BASIC_TOKEN -r gitlab-conan-project
+    conan user pallp -p $NORBIT_GITLAB_BASIC_TOKEN -r gitlab
 }
 
