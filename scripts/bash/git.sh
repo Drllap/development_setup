e0c0398f07c9732af049e56fcf7ac07049b805d6
Pall Palsson <drllap@gmail.com>
2026-02-18 19:47:43 +0000

    Bash: Few fixes to wtf command
    * Fix xargs warning after updating it. Removing the `-n1` argument.
      Doesn't make sense when using -I{}
    * Handle when the fzf command is canceled by user
    * Remove commented out code

--- scripts/bash/git.sh
+++ scripts/bash/git.sh
@@ -5,13 +5,18 @@ function wtf {
   repos=( \
   )
   function run_fzf {
-    printf "%s\n" "${repos[@]}" | xargs -n1 -I{} sh -c '
+    printf "%s\n" "${repos[@]}" | xargs -I{} sh -c '
       repo="{}"
       git -C "$repo" worktree list
       ' | fzf --cycle --bind 'enter:become(echo {1})'
   }
 
   # echo "$(run_fzf)"
-  builtin cd "$(run_fzf)";
+  dest=$(run_fzf)
+  if [[ -n $dest ]]; then
+    builtin cd $dest;
+  else
+    echo "empty"
+  fi
 }
 
