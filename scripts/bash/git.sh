295162d1025f90a3d860f3f13f750ac98e42ccfc
Pall Palsson <drllap@gmail.com>
2026-03-12 14:34:26 +0000

    Bash: Update the wtf function
    * Show the branch in the fzf selection if available
    * Dynamically resize/move the preview window depending on space
    * Remove hard-coded substring from the start of the paths in the fzf
      selection list

--- scripts/bash/git.sh
+++ scripts/bash/git.sh
@@ -6,26 +6,78 @@ function wtf {
   )
 
   function run_fzf {
+    local prefix_to_remove="^/home/palli/dev/"
+    local preview_window='right:45%:wrap'
+
+    if [[ ${COLUMNS:-0} -lt 180 ]]; then
+      preview_window='right:35%:wrap'
+    fi
+
+    if [[ ${COLUMNS:-0} -lt 140 ]]; then
+      preview_window='down:40%:wrap'
+    fi
 
     for repo in "${repos[@]}"; do
       git -C "$repo" worktree list --porcelain 2>/dev/null \
-        | awk '/^worktree /{print $2}'
+        | awk -v prefix_to_remove="$prefix_to_remove" '
+            /^worktree / {
+              if (path != "") {
+                display = path
+                sub(prefix_to_remove, "", display)
+                print path "\t" display "\t" branch
+              }
+              path = substr($0, 10)
+              branch = ""
+              next
+            }
+
+            /^branch / {
+              branch = $2
+              sub(/^refs\/heads\//, "", branch)
+              next
+            }
+
+            END {
+              if (path != "") {
+                display = path
+                sub(prefix_to_remove, "", display)
+                print path "\t" display "\t" branch
+              }
+            }
+          '
     done \
+    | awk -F '\t' '
+        {
+          rows[NR] = $0
+          if (length($2) > max_display_width) {
+            max_display_width = length($2)
+          }
+        }
+
+        END {
+          for (i = 1; i <= NR; ++i) {
+            split(rows[i], fields, "\t")
+            printf "%s\t%-*s\t%s\n", fields[1], max_display_width, fields[2], fields[3]
+          }
+        }
+      ' \
     | fzf --cycle \
-        --preview 'wt={}; 
+        --delimiter=$'\t' \
+        --with-nth=2,3 \
+        --preview 'wt={1}; 
                    if [ -d "$wt/.git" ] || [ -f "$wt/.git" ]; then
                      git -C "$wt" -c color.status=always status --short --branch 2>/dev/null \
                        | (command -v bat >/dev/null && bat --paging=never --plain --language=diff || cat)
                    else
                      echo "Not a git worktree: $wt"
                    fi' \
-        --preview-window 'right:60%:wrap' \
-        --bind 'enter:become(echo {})'
+        --preview-window "$preview_window" \
+        --bind 'enter:become(echo {1})'
   }
 
   dest=$(run_fzf)
   if [[ -n $dest ]]; then
-    builtin cd $dest;
+    builtin cd "$dest";
   else
     echo "empty"
   fi
